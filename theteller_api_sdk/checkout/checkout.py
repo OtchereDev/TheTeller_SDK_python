b48bf024b242b6214ffbd2ea489e02f8271548c8
Oliver Otchere <oliverotchere4@gmail.com>
2021-09-11 12:31:45 +0000

    update checkout functionalty

--- theteller_api_sdk/checkout/checkout.py
+++ theteller_api_sdk/checkout/checkout.py
@@ -1,12 +1,11 @@
 
 import typing
 from core import core
-from random import choices
-from string import digits
+from helpers.helpers import generateHeader, generateTransactionId
 from errors import errors
 from validators import email,url
 import requests
-import base64
+
 
 class Checkout():
     def __init__(self,client:core.Client)->None:
@@ -15,16 +14,12 @@ class Checkout():
 
         self.client=client
 
-    def generateTransactionId() -> str:
-        return choices(digits,k=12)
-
-
     def generateRequestBody(self,description,amount,redirect_url,customer_email) -> "dict[str, typing.Any]":
         return {
             "merchant_id":self.client.merchant_id,
-            "transaction_id":self.generateTransactionId(),
+            "transaction_id":generateTransactionId(),
             "desc":description,
-            "amount":amount,
+            "amount":f"{amount}",
             "redirect_url":redirect_url,
             "email":customer_email
         }
@@ -38,35 +33,30 @@ class Checkout():
         if type(amount) not in [int,float]:
             raise errors.InvalidAmountType
 
-        if not email.email(customer_email):
+        if not email(customer_email):
             raise errors.InvalidEmail
 
-        if not url.url(redirect_url):
+        if not url(redirect_url):
             raise errors.InvalidRedirectUrl
 
-        if type(amount) == int: 
-            new_amount = int(amount)  
-        else:
-             new_amount=float(amount)
+        if len(description)==0:
+            raise errors.DescriptionRequired
 
         baseUri=self.client.environment.getBaseUrl()
+        endpoint="checkout/initiate"
 
-        headers={
-            "content-type":"application/json",
-            "Authorization": f"Basic {base64.b64encode(f'{self.client.apiuser}:{self.client.API_Key}')}",
-            "Cache-Control": "no-cache"
-        }
+        headers= generateHeader(self)
 
-        request_data=self.generateRequestBody(description,new_amount,redirect_url,customer_email)
+        request_data=self.generateRequestBody(description,amount,redirect_url,customer_email)
 
-        request = requests.post(f'{baseUri}/checkout/initiate',json=request_data,headers=headers)
+        request = requests.post(f'{baseUri}/{endpoint}',json=request_data,headers=headers)
 
         if (request.status_code==200):
             data=request.json()
             return {
-                "status":data.status,
-                "token": data.token,
-                "checkout_url": data.checkout_url
+                "status":data.get("status"),
+                "token": data.get("token"),
+                "checkout_url": data.get("checkout_url")
             }
         
         return  {
@@ -89,11 +79,11 @@ class Checkout():
         if request.status_code==200:
             data=request.json()
             return {
-                "status":   data.status,
-                "message": data.reason,
-                "r_switch": data.r_switch,
-                "subscriber_number": data.subscriber_number,
-                "amount": data.amount
+                "status":   data.get("status"),
+                "message": data.get("reason"),
+                "r_switch": data.get("r_switch"),
+                "subscriber_number": data.get("subscriber_number"),
+                "amount": data.get("amount")
             }
 
         else:
